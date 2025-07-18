d9c78032d0727eaad1e390ae4140593a6683fc40
davidfoo007 <tech@myunilah.com>
2025-07-18 11:20:09 +0800

    fixed import the script

--- docs/scripts/regenerate-index.js
+++ docs/scripts/regenerate-index.js
@@ -1,5 +1,5 @@
-const fs = require("node:fs").promises;
-const path = require("node:path");
+import fs from "node:fs";
+import path from "node:path";
 
 class BlogIndexGenerator {
     constructor() {
